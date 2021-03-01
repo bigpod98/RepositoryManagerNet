c8689567f3dae9394ce90e40323e19c43855db52
Primož Ajdišek <ajdisek.primoz@gmail.com>
2021-03-01 23:54:01 +0100

    adding deregister and modify controllers

---
+++ RepositoryManagerNet.API/Controllers/deregisterRepoController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RepositoryManagerNet.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class deregisterRepoController : ControllerBase
+    {
+        [HttpDelete]
+        public string Delete([FromBody] Models.RepoData RepositoryData)
+        {
+            return RepositoryData.Name;
+        }
+    }
+}
---
+++ RepositoryManagerNet.API/Controllers/modifyRepoController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RepositoryManagerNet.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class modifyRepoController : ControllerBase
+    {
+        [HttpPut("repoID")]
+        public string Put(string repoID, [FromBody] Models.RepoData RepositoryData)
+        {
+
+            
+            return RepositoryData.Name;
+        }
+    }
+}
