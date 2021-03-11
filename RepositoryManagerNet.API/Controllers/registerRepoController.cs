5f586a3065e7c61b87bb7dc1b155530ba56d4556
Primož Ajdišek <ajdisek.primoz@gmail.com>
2021-03-12 00:58:19 +0100

    added code for pushing

--- RepositoryManagerNet.API/Controllers/registerRepoController.cs
+++ RepositoryManagerNet.API/Controllers/registerRepoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
 
 namespace RepositoryManagerNet.API.Controllers
 {
@@ -14,7 +15,17 @@ namespace RepositoryManagerNet.API.Controllers
         [HttpPost]
         public string Post([FromBody] Models.RepoData RepositoryData)
         {
-            //add to database
+            MySqlConnection con = new MySqlConnection("Server=10.152.183.94;Database=repomanager;Uid=RepoManager;Pwd=RepoManager;");
+
+            string Command = $"INSERT INTO Repositories (Name, PackageType, BaseDomain) VALUES (@Name, @PackageType, @BaseDomain)";
+            
+            MySqlCommand cmd = new MySqlCommand(Command, con);
+            cmd.Parameters.AddWithValue("@Name", RepositoryData.Name);
+            cmd.Parameters.AddWithValue("@PackageType", RepositoryData.PackageType);
+            cmd.Parameters.AddWithValue("@BaseDomain", RepositoryData.BaseDomain);
+            con.Open();
+
+            cmd.ExecuteNonQuery();
 
             return RepositoryData.Name;
         }
