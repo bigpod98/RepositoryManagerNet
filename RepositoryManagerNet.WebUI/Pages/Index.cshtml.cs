eaf883ccfe88342e8641ef2e95b011dc94ad4ea3
Primož Ajdišek <ajdisek.primoz@gmail.com>
2021-03-12 01:00:22 +0100

    added code for properties and onpost Call to APi

--- RepositoryManagerNet.WebUI/Pages/Index.cshtml.cs
+++ RepositoryManagerNet.WebUI/Pages/Index.cshtml.cs
@@ -1,16 +1,42 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
 
 namespace RepositoryManagerNet.WebUI.Pages
 {
     public class IndexModel : PageModel
     {
+        private static HttpClient client = new HttpClient();
+
+        [BindProperty]
+        public string Name {get;set;}
+        [BindProperty]
+        public string PackageType { get; set; }
+        [BindProperty]
+        public string BaseDomain {get;set;}
+        
+        public string[] PackageManagerTypes = new[] {"APT", "Pacman", "RPM"};
         public void OnGet()
         {
+        }
+
+        public async Task OnPost()
+        {
+            Models.RepoData Repository = new Models.RepoData(0, Name,PackageType,BaseDomain);
+            var Content = new StringContent(JsonSerializer.Serialize(Repository),
+                                            Encoding.UTF8,
+                                            "application/json");
+            client.BaseAddress = new Uri("http://127.0.0.1:6000");
+            HttpResponseMessage responseMessage = await client.PostAsync("api/register", Content); 
+            
         }
     }
 }
