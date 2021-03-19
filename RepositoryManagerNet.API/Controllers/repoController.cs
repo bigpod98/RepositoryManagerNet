5712d693d25772bb981fcfc02c9e67807ce57db2
Primož Ajdišek <ajdisek.primoz@gmail.com>
2021-03-19 23:59:47 +0100

    added code for get single repo info and for get multiple repo info

--- RepositoryManagerNet.API/Controllers/repoController.cs
+++ RepositoryManagerNet.API/Controllers/repoController.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
 
 namespace RepositoryManagerNet.API.Controllers
 {
@@ -14,16 +16,55 @@ namespace RepositoryManagerNet.API.Controllers
         [HttpGet("repoID")]
         public string Get(string repoID)
         {
-            //get single instance from database
-            return "";
+            MySqlConnection con = new MySqlConnection("Server=10.152.183.94;Database=repomanager;Uid=RepoManager;Pwd=RepoManager;");
+
+            string Command = $"SELECT * FROM Repositories WHERE ID=@repoID;";
+
+            MySqlCommand cmd = new MySqlCommand(Command, con);
+            if (Int32.TryParse(repoID, out _))
+            {
+                cmd.Parameters.AddWithValue("@repoID", repoID);
+            }
+            else
+            {
+                return "Error: RepoID not a intiger";
+            }
+            con.Open();
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            Models.RepoData repo;
+            if (reader.Read())
+            {
+                repo = new Models.RepoData(reader.GetInt32("ID"), reader.GetString("Name"),
+                    reader.GetString("PackageType"), reader.GetString("BaseDomain"));
+            }
+            else{
+                repo = new Models.RepoData();
+            }
+            return JsonSerializer.Serialize(repo);
         }
 
         [HttpGet()]
         public List<Models.RepoData> Get()
         {
-            //get all instances from database
+            MySqlConnection con = new MySqlConnection("Server=10.152.183.94;Database=repomanager;Uid=RepoManager;Pwd=RepoManager;");
 
-            return new List<Models.RepoData>();
+            string Command = $"SELECT * FROM Repositories;";
+
+            MySqlCommand cmd = new MySqlCommand(Command, con);
+            con.Open();
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            List<Models.RepoData> repos = new List<Models.RepoData>();
+            while (reader.Read())
+            {
+                repos.Add(new Models.RepoData(reader.GetInt32("ID"), reader.GetString("Name"),
+                    reader.GetString("PackageType"), reader.GetString("BaseDomain")));
+            }
+
+            return repos;
         }
     }
 }
