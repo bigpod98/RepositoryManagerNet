e6d58697f03631660801a41781b7de7704cb0c5e
Primož Ajdišek <ajdisek.primoz@gmail.com>
2021-04-10 01:03:33 +0200

    Corrected the connection string building

--- RepositoryManagerNet.API/Controllers/deregisterRepoController.cs
+++ RepositoryManagerNet.API/Controllers/deregisterRepoController.cs
@@ -15,7 +15,18 @@ namespace RepositoryManagerNet.API.Controllers
         [HttpDelete]
         public string Delete([FromBody] Models.RepoData RepositoryData)
         {
-            MySqlConnection con = new MySqlConnection("Server=10.152.183.94;Database=repomanager;Uid=RepoManager;Pwd=RepoManager;");
+            // MySqlConnection con = new MySqlConnection("Server=10.152.183.94;Database=repomanager;Uid=RepoManager;Pwd=RepoManager;");
+
+            MySqlConnectionStringBuilder conBuilder = new MySqlConnectionStringBuilder()
+            {
+                Server=Settings.MYSQL_IP,
+                Database=Settings.MYSQL_DBName,
+                UserID=Settings.MYSQL_UserName,
+                Password=Settings.MYSQL_Password,
+                Port=Convert.ToUInt32(Settings.MYSQL_PORT) 
+            };
+
+            MySqlConnection con = new MySqlConnection(conBuilder.GetConnectionString(true));
 
             string Command = $"DELETE FROM Repositories WHERE ID=@RepoID";
             
--- RepositoryManagerNet.API/Controllers/repoController.cs
+++ RepositoryManagerNet.API/Controllers/repoController.cs
@@ -48,7 +48,18 @@ namespace RepositoryManagerNet.API.Controllers
         [HttpGet()]
         public List<Models.RepoData> Get()
         {
-            MySqlConnection con = new MySqlConnection("Server=10.152.183.94;Database=repomanager;Uid=RepoManager;Pwd=RepoManager;");
+            // MySqlConnection con = new MySqlConnection("Server=10.152.183.94;Database=repomanager;Uid=RepoManager;Pwd=RepoManager;");
+
+            MySqlConnectionStringBuilder conBuilder = new MySqlConnectionStringBuilder()
+            {
+                Server=Settings.MYSQL_IP,
+                Database=Settings.MYSQL_DBName,
+                UserID=Settings.MYSQL_UserName,
+                Password=Settings.MYSQL_Password,
+                Port=Convert.ToUInt32(Settings.MYSQL_PORT) 
+            };
+
+            MySqlConnection con = new MySqlConnection(conBuilder.GetConnectionString(true));
 
             string Command = $"SELECT * FROM Repositories;";
 
