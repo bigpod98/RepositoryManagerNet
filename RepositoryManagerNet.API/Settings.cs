b2131e62c94d692fc098d22979aae57f9e8e6d55
Primož Ajdišek <ajdisek.primoz@gmail.com>
2021-04-10 23:23:52 +0200

    finished the code for pulling settings from DB

--- RepositoryManagerNet.API/Settings.cs
+++ RepositoryManagerNet.API/Settings.cs
@@ -1,13 +1,13 @@
 using System;
 using static System.Environment;
-using MySql.Data.Common;
+using MySql.Data.MySqlClient;
 
 namespace RepositoryManagerNet.API
 {
     public static class Settings
     {
         public static string MYSQL_DBName;
-        public static string MYSQL_UserName; 
+        public static string MYSQL_UserName;
         public static string MYSQL_Password;
         public static string MYSQL_IP;
         public static string MYSQL_PORT;
@@ -23,29 +23,30 @@ namespace RepositoryManagerNet.API
 
         public static void getSettingsENV()
         {
+            string noValueENV = GetEnvironmentVariable("dspldfsgjdafdgsdfagrf");
             MYSQL_DBName = GetEnvironmentVariable("MYSQL_DATABASE_NAME");
             MYSQL_UserName = GetEnvironmentVariable("MYSQL_USERNAME");
             MYSQL_Password = GetEnvironmentVariable("MYSQL_PASSWORD");
             MYSQL_IP = GetEnvironmentVariable("MYSQL_IO");
             MYSQL_PORT = GetEnvironmentVariable("MYSQL_PORT");
 
-            if(MYSQL_DBName == " ")
+            if (MYSQL_DBName == noValueENV)
             {
                 Console.WriteLine("No Database name defined");
             }
-            if(MYSQL_UserName == " ")
+            if (MYSQL_UserName == noValueENV)
             {
                 Console.WriteLine("No Username defined");
             }
-            if(MYSQL_Password == " ")
+            if (MYSQL_Password == noValueENV)
             {
                 Console.WriteLine("No Password defined");
             }
-            if(MYSQL_IP == " ")
+            if (MYSQL_IP == noValueENV)
             {
                 Console.WriteLine("No Database IP defined");
             }
-            if(MYSQL_PORT == " ")
+            if (MYSQL_PORT == noValueENV)
             {
                 Console.WriteLine("No Database port defined");
             }
@@ -53,48 +54,67 @@ namespace RepositoryManagerNet.API
 
         public static void getSettingsDB()
         {
-            
-
-            string storageKey = "";
-            switch(storageKey)
+            MySqlConnectionStringBuilder conBuilder = new MySqlConnectionStringBuilder()
             {
-                case "StorageClass":
-                StorageClass = "";
-                break;
-                case "KubernetesNamespace":
-                KubernetesNamespace = "";
-                break;
-                case "APTRepository":
-                ContainerImages.APTRepository = "";
-                break;
-                case "RPMRepository":
-                ContainerImages.RPMRepository = "";
-                break;
-                case "PacmanRepository":
-                ContainerImages.PacmanRepository = "";
-                break;
+                Server = Settings.MYSQL_IP,
+                Database = Settings.MYSQL_DBName,
+                UserID = Settings.MYSQL_UserName,
+                Password = Settings.MYSQL_Password,
+                Port = Convert.ToUInt32(Settings.MYSQL_PORT)
+            };
+
+            MySqlConnection con = new MySqlConnection(conBuilder.GetConnectionString(true));
+
+            string Command = $"SELECT setting, vsetting FROM Settings;";
+
+            MySqlCommand cmd = new MySqlCommand(Command, con);
+            con.Open();
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string storageKey = reader.GetString("setting");
+                switch (storageKey)
+                {
+                    case "StorageClass":
+                        StorageClass = reader.GetString("vsetting");
+                        break;
+                    case "KubernetesNamespace":
+                        KubernetesNamespace = reader.GetString("vsetting");
+                        break;
+                    case "APTRepository":
+                        ContainerImages.APTRepository = reader.GetString("vsetting");
+                        break;
+                    case "RPMRepository":
+                        ContainerImages.RPMRepository = reader.GetString("vsetting");
+                        break;
+                    case "PacmanRepository":
+                        ContainerImages.PacmanRepository = reader.GetString("vsetting");
+                        break;
+                }
             }
 
-            if(StorageClass == "")
+            if (StorageClass == "")
             {
                 Console.WriteLine("No Storage Class defined");
             }
-            if(KubernetesNamespace == "")
+            if (KubernetesNamespace == "")
             {
                 Console.WriteLine("No Kubernetes Namespace defined");
-            }  
-            if(ContainerImages.APTRepository == "")
+            }
+            if (ContainerImages.APTRepository == "")
             {
                 Console.WriteLine("No APT Repository image defined");
-            }  
-            if(ContainerImages.RPMRepository == "")
+            }
+            if (ContainerImages.RPMRepository == "")
             {
                 Console.WriteLine("No RPM Repository image defined");
-            }  
-            if(ContainerImages.PacmanRepository == "")
+            }
+            if (ContainerImages.PacmanRepository == "")
             {
                 Console.WriteLine("No Pacman Repository image defined");
-            }            
+            }
         }
     }
 }
