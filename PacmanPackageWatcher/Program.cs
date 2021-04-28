683226563c6f54e5ab4e84c5a2890afeb41c3bb6
Primož Ajdišek <ajdisek.primoz@gmail.com>
2021-04-29 00:03:32 +0200

    added 3 projects and basic code for them

---
+++ PacmanPackageWatcher/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+string WatchLocation = "/Watch";
+string repoLocation = "/repository";
+DirectoryInfo WatchDir = new DirectoryInfo(WatchLocation);
+while (true)
+{
+    List<FileInfo> files = WatchDir.EnumerateFiles().ToList();
+
+    if (files.Count == 0)
+    {
+        continue;
+    }
+    if (files.Count > 0)
+    {
+        foreach (FileInfo i in files)
+        {
+            Console.WriteLine(i.FullName);
+            //run repoadd
+            File.Copy(i.FullName, $"{repoLocation}/{i.Name}");
+
+        }
+    }
+
+    System.Threading.Thread.Sleep(2000);
+}
