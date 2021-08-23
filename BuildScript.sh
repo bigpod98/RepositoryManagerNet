c7934d3782cd6f98bd1686876733cfc9ca46e5c3
Primož Ajdišek <ajdisek.primoz@hotmail.com>
2021-08-23 23:00:33 +0200

    small fixes to dockerfiles

--- BuildScript.sh
+++ BuildScript.sh
@@ -1,9 +1,9 @@
-docker build -t ghcr.io/bigpod98/aptpackagewatcher:latest -f DockerFiles/APTPackageWatcher/Dockerfile .
-docker build -t ghcr.io/bigpod98/databasedeployment:latest -f DockerFiles/databaseDeployment/Dockerfile .
-docker build -t ghcr.io/bigpod98/pacmanpackagewatcher:latest -f DockerFiles/PacmanPackageWatcher/Dockerfile .
-docker build -t ghcr.io/bigpod98/repositorymanagernetapi:latest -f DockerFiles/RepositoryManagerNet.API/Dockerfile .
-docker build -t ghcr.io/bigpod98/repositorymanagernetwebui:latest -f DockerFiles/RepositoryManagerNet.WebUI/Dockerfile .
-docker build -t ghcr.io/bigpod98/rpmpackagewatcher:latest -f DockerFiles/RPMPackageWatcher/Dockerfile .
+docker build -t ghcr.io/bigpod98/aptpackagewatcher:latest -f DockerFiles/APTPackageWatcher/Dockerfile . --no-cache
+docker build -t ghcr.io/bigpod98/databasedeployment:latest -f DockerFiles/databaseDeployment/Dockerfile . --no-cache
+docker build -t ghcr.io/bigpod98/pacmanpackagewatcher:latest -f DockerFiles/PacmanPackageWatcher/Dockerfile . --no-cache
+docker build -t ghcr.io/bigpod98/repositorymanagernetapi:latest -f DockerFiles/RepositoryManagerNet.API/Dockerfile . --no-cache
+docker build -t ghcr.io/bigpod98/repositorymanagernetwebui:latest -f DockerFiles/RepositoryManagerNet.WebUI/Dockerfile . --no-cache
+docker build -t ghcr.io/bigpod98/rpmpackagewatcher:latest -f DockerFiles/RPMPackageWatcher/Dockerfile . --no-cache
 docker push ghcr.io/bigpod98/aptpackagewatcher:latest
 docker push ghcr.io/bigpod98/databasedeployment:latest
 docker push ghcr.io/bigpod98/pacmanpackagewatcher:latest
