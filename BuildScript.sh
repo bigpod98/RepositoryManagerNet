f63f9bf5ae27c8b19ce02d5c5dc2739725fcaf40
Primož Ajdišek <ajdisek.primoz@hotmail.com>
2021-08-26 02:27:53 +0200

    added upload api to buildscript

--- BuildScript.sh
+++ BuildScript.sh
@@ -2,11 +2,13 @@ docker build -t ghcr.io/bigpod98/aptpackagewatcher:latest -f DockerFiles/APTPack
 docker build -t ghcr.io/bigpod98/databasedeployment:latest -f DockerFiles/databaseDeployment/Dockerfile . --no-cache
 docker build -t ghcr.io/bigpod98/pacmanpackagewatcher:latest -f DockerFiles/PacmanPackageWatcher/Dockerfile . --no-cache
 docker build -t ghcr.io/bigpod98/repositorymanagernetapi:latest -f DockerFiles/RepositoryManagerNet.API/Dockerfile . --no-cache
+docker build -t ghcr.io/bigpod98/repositorymanagernetuploadapi:latest -f DockerFiles/RepositoryManagerNet.UploadAPI/Dockerfile . --no-cache
 docker build -t ghcr.io/bigpod98/repositorymanagernetwebui:latest -f DockerFiles/RepositoryManagerNet.WebUI/Dockerfile . --no-cache
 docker build -t ghcr.io/bigpod98/rpmpackagewatcher:latest -f DockerFiles/RPMPackageWatcher/Dockerfile . --no-cache
 docker push ghcr.io/bigpod98/aptpackagewatcher:latest
 docker push ghcr.io/bigpod98/databasedeployment:latest
 docker push ghcr.io/bigpod98/pacmanpackagewatcher:latest
 docker push ghcr.io/bigpod98/repositorymanagernetapi:latest
+docker push ghcr.io/bigpod98/repositorymanagernetuploadapi:latest
 docker push ghcr.io/bigpod98/repositorymanagernetwebui:latest
 docker push ghcr.io/bigpod98/rpmpackagewatcher:latest
