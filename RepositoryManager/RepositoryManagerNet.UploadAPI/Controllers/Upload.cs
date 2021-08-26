caf02c234519d54856d9a1607fb1c4ded89d30fb
Primož Ajdišek <ajdisek.primoz@hotmail.com>
2021-08-26 02:24:22 +0200

    added upload API in seperate project

---
+++ RepositoryManager/RepositoryManagerNet.UploadAPI/Controllers/Upload.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RepositoryManagerNet.UploadAPI.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class Upload : ControllerBase
+{
+    // POST <UploadController>
+    [HttpPost]
+    public string Post(IFormFile package)
+    {
+        string returnval = $"{package.Name} -- {package.ContentType} -- {package.ToString()}";
+        if (package.Length > 0)
+        {
+            var filePath = Path.GetTempFileName();
+
+            using (var stream = System.IO.File.Create(filePath))
+            {
+                package.CopyTo(stream);
+                using (var fileStream = System.IO.File.Create(""))
+                {
+                    package.CopyTo(fileStream);
+                }
+            }
+        }
+        return returnval;
+    }
+}
