da35d1fce70f05255d0758e2da2a8fe508a9dcd2
Primož Ajdišek <ajdisek.primoz@gmail.com>
2021-08-27 01:31:17 +0200

    another fix

--- BuildScript.sh
+++ BuildScript.sh
@@ -5,9 +5,9 @@ docker build -t ghcr.io/bigpod98/repositorymanagernetapi:latest -f DockerFiles/R
 docker build -t ghcr.io/bigpod98/repositorymanagernetuploadapi:latest -f DockerFiles/RepositoryManagerNet.UploadAPI/Dockerfile . --no-cache
 docker build -t ghcr.io/bigpod98/repositorymanagernetwebui:latest -f DockerFiles/RepositoryManagerNet.WebUI/Dockerfile . --no-cache
 docker build -t ghcr.io/bigpod98/rpmpackagewatcher:latest -f DockerFiles/RPMPackageWatcher/Dockerfile . --no-cache
-docker build -t ghcr.io/bigpod98/rmn-aptrepoinit:latest -f Dockerfile/Scripts/APTRepoInit/Dockerfile . --no-cache
-docker build -t ghcr.io/bigpod98/rmn-rpmrepoinit:latest -f Dockerfile/Scripts/RPMRepoInit/Dockerfile . --no-cache
-docker build -t ghcr.io/bigpod98/rmn-pacmanrepoinit:latest -f Dockerfile/Scripts/PacmanRepoInit/Dockerfile . --no-cache
+docker build -t ghcr.io/bigpod98/rmn-aptrepoinit:latest -f DockerFiles/Scripts/APTRepoInit/Dockerfile . --no-cache
+docker build -t ghcr.io/bigpod98/rmn-rpmrepoinit:latest -f DockerFiles/Scripts/RPMRepoInit/Dockerfile . --no-cache
+docker build -t ghcr.io/bigpod98/rmn-pacmanrepoinit:latest -f DockerFiles/Scripts/PacmanRepoInit/Dockerfile . --no-cache
 docker push ghcr.io/bigpod98/aptpackagewatcher:latest
 docker push ghcr.io/bigpod98/databasedeployment:latest
 docker push ghcr.io/bigpod98/pacmanpackagewatcher:latest
