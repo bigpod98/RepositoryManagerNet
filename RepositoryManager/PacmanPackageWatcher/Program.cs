705c5f6a71242ecfeb91030dcb9985005ac17698
Primož Ajdišek <ajdisek.primoz@gmail.com>
2021-08-29 02:40:07 +0200

    added pacman add to repo code that should work

--- RepositoryManager/PacmanPackageWatcher/Program.cs
+++ RepositoryManager/PacmanPackageWatcher/Program.cs
@@ -2,9 +2,11 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.IO;
+using System.Diagnostics;
 
 string WatchLocation = "/Watch";
 string repoLocation = "/repository";
+string name = Environment.GetEnvironmentVariable("REPONAME");
 DirectoryInfo WatchDir = new DirectoryInfo(WatchLocation);
 while (true)
 {
@@ -19,8 +21,13 @@ while (true)
         foreach (FileInfo i in files)
         {
             Console.WriteLine(i.FullName);
-            //run repoadd
+
+            Process p = new Process();
+
             File.Copy(i.FullName, $"{repoLocation}/{i.Name}");
+
+            p.StartInfo.FileName = "repo-add";
+            p.StartInfo.Arguments = $"{repoLocation}/{name}.db.tar.gz /{repoLocation}/{i.Name}";
 
         }
     }
